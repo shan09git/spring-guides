6c9f6e403156fa8482031bdea112b3033aa5d207
shan09git <shanjob09@gmail.com>
2025-01-19 21:05:38 -0500

    changed all methods from public to package level and changed update method.

--- payroll-service/src/main/java/service/PayrollServiceApplication.java
+++ payroll-service/src/main/java/service/PayrollServiceApplication.java
@@ -13,10 +13,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -50,7 +52,9 @@ class EmployeeNotFound extends RuntimeException {
     Long id;
     String message;
 
-    public EmployeeNotFound(Long id, String message) {
+    EmployeeNotFound(Long id, String message) {
+        this.id = id;
+        this.message = message;
     }
 
 }
@@ -60,7 +64,7 @@ class AppException {
 
 
     @ExceptionHandler(EmployeeNotFound.class)
-    public ProblemDetail handleException(EmployeeNotFound employeeNotFound) {
+    ProblemDetail handleException(EmployeeNotFound employeeNotFound) {
         var problemDetails = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, employeeNotFound.message);
         problemDetails.setType(URI.create("http://localhost:8080/error"));
         problemDetails.setTitle("error");
@@ -76,13 +80,13 @@ class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
+    EmployeeController(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
     @GetMapping("/")
     @ResponseBody
-    public Collection<Employee> all() {
+    Collection<Employee> all() {
         var emps = this.employeeRepository.findAll();
         if (emps.isEmpty()) {
             throw new EmployeeNotFound(0l, "no employees Found");
@@ -93,28 +97,39 @@ class EmployeeController {
 
     @GetMapping("/{id}")
     @ResponseBody
-    public ResponseEntity<Employee> getOne(@PathVariable Long id) {
+    ResponseEntity<Employee> getOne(@PathVariable Long id) {
         return this.employeeRepository
                 .findById(id)
                 .map(ResponseEntity::ok)
-                .orElseThrow(()-> new EmployeeNotFound(id, "Employee Not Found"));
+                .orElseThrow(() -> new EmployeeNotFound(id, "Employee Not Found"));
     }
 
     @PostMapping("/new")
     @ResponseBody
-    public Employee newEmployee(@RequestBody Employee employee) {
+    Employee newEmployee(@RequestBody Employee employee) {
         return Optional.of(this.employeeRepository.save(employee)).orElse(null);
     }
 
     @PutMapping("/update/{id}")
     @ResponseBody
-    public Employee updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
-        return Optional.of(this.employeeRepository.save(employee)).orElse(null);
+    Employee updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+        return this.employeeRepository.findById(id)
+                .map(emp -> {
+                    emp.setName(employee.getName());
+                    emp.setTitle(employee.getTitle());
+                    emp.setSalary(employee.getSalary());
+                    return employeeRepository.save(emp);
+                })
+                .orElseThrow(() -> new EmployeeNotFound(id, id + " id not found to update"));
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteEmployee(@PathVariable Long id) {
-        this.employeeRepository.deleteById(id);
+    @ResponseBody
+    @Transactional
+    String deleteEmployee(@PathVariable Long id) {
+        this.employeeRepository
+                .deleteById(id);
+        return "deleted employee of Id" + id;
     }
 
 }
@@ -140,9 +155,13 @@ class Employee {
 
     Double salary;
 
-    public Employee(String name, String title, Double salary) {
+    Employee(String name, String title, Double salary) {
         this.name = name;
         this.title = title;
         this.salary = salary;
+    }
+
+    Employee(Long id) {
+        this.id = id;
     }
 }
